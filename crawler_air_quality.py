720c4ac0c0020b5a23cc41376f3968b98f030ca8
jiayi-wei <weijiayi1217@gmail.com>
2016-12-05 15:39:17 +0800

    Add files via upload

--- crawler_air_quality.py
+++ crawler_air_quality.py
@@ -8,40 +8,82 @@ import pypinyin
 import time
 import datetime
 
+def crawl_air_quality_beijing():
+    url = "http://www.aqistudy.cn/api/getdata_citydetailinfo_memcache.php"
+    city = "北京"
+    city_encode = base64.b64encode(base64.b64encode(base64.b64encode(bytes(city,"UTF-8"))))
+    data = {'city': city_encode }
+    r = requests.post(url, data=data)
+    r_str=base64.b64decode(base64.b64decode(r.text)).decode('ascii')
+    air={}
+    nor_len=r_str.find("}}",0,len(r_str))+2
+    if(nor_len != len(r_str)):
+        r_str=r_str[0:nor_len]
+    air=json.loads(r_str)
+    air_quality_all=air['rows']
+    now=datetime.datetime.now()
+    for air_quality in air_quality_all:
+        s=air_quality['pointname']
+        s_name=""
+        for s_temp in lazy_pinyin(s):
+            s_name+=s_temp
+        path="D:\\air_quality\\%s.txt"%s_name
+        if air_quality['aqi']=="0":
+            air_quality['aqi']="10000"
+        if air_quality['pm2_5']=="0":
+            air_quality['pm2_5']="10000"
+        if air_quality['pm10']=="0":
+            air_quality['pm10']="10000"
+        with open(path,'a') as f:
+            f.write(air_quality['aqi']+"   "+air_quality['pm2_5']+"   "+air_quality['pm10']+"   "+now.strftime('%Y%m%d%H%M%S')+'\n')
+    try:
+        conn=pymysql.connect(user='root',passwd='123123',host='10.102.0.194',db='image')
+        cur = conn.cursor()
+    except:
+        print("未能连接数据库")
+        time.sleep(300) 
+    for air_quality in air_quality_all:
+        s=air_quality['pointname']
+        s_name=""
+        for s_temp in lazy_pinyin(s):
+            s_name+=s_temp
+        cur.execute("insert into all_quality(Position, Location, time, AQI, PM25, PM10) VALUES('%s','%s','%s','%d','%d','%d')"%(s_name,air_quality['longitude'][0:6]+"_0"+air_quality['latitude'][0:5],now.strftime('%Y%m%d%H%M%S'),int(air_quality['aqi']),int(air_quality['pm2_5']),int(air_quality['pm10'])))
+        conn.commit()            
+    cur.close()
+    conn.close()
+
+def crawl_air_quality_bupt():
+    url="http://220.195.3.56:7088/SensorApp_bupt/user/getLogin"
+    para={'username':'bupt', 'password':'bupt'}
+    now=datetime.datetime.now()
+    timestamp=now.strftime('%Y-%m-%d %H')+':00:00'
+    url_home="http://220.195.3.56:7088/SensorApp_bupt/monitoringDiagram/queryPointListByMeasurePointId"
+    para_home={'id':'1243', 'timestamp':timestamp, 'page':'1'}
+    s=requests.Session()
+    s.post(url, data=para)
+    r=s.post(url_home, data=para_home)
+    da=json.loads(r.text)
+    data=da['list'][0]
+    path="D:\\air_quality\\bupt.txt"
+    with open(path, 'a') as f:
+        f.write(str(round(data['pm25']))+"   "+now.strftime('%Y%m%d%H%M%S')+'\n')
+    try:
+        conn=pymysql.connect(user='root',passwd='123123',host='10.102.0.194',db='image')
+        cur = conn.cursor()
+    except:
+        print("未能连接数据库")
+        time.sleep(300)
+    cur.execute("insert into all_quality(Position, Location, time, PM25) VALUES('%s','%s','%s','%d')"%('bupt',str(data['longitude'])[0:6]+"_0"+str(data['latitude'])[0:5],now.strftime('%Y%m%d%H%M%S'),round(data['pm25'])))
+    conn.commit()            
+    cur.close()
+    conn.close()
+    
+
 def crawl_air_quality(inc=1800):
-    while True:
-        url = "http://www.aqistudy.cn/api/getdata_citydetailinfo_memcache.php"
-        city = "北京"
-        city_encode = base64.b64encode(base64.b64encode(base64.b64encode(bytes(city,"UTF-8"))))
-        data = {'city': city_encode }
-        r = requests.post(url, data=data)
-        r_str=base64.b64decode(base64.b64decode(r.text)).decode('ascii')
-        air={}
-        try:
-            air=json.loads(r_str)
-        except:
-            time.sleep(300)
-            continue
-        air_quality_all=air['rows']
-        try:
-            conn=pymysql.connect(user='root',passwd='123123',host='10.102.0.194',db='image')
-            cur = conn.cursor()
-        except:
-            time.sleep(300)
-            continue;
-        now=datetime.datetime.now() 
-        for air_quality in air_quality_all:
-            s=air_quality['pointname']
-            s_name=""
-            for s_temp in lazy_pinyin(s):
-                s_name+=s_temp
-            cur.execute("insert into all_quality(Position, Location, time, AQI, PM25, PM10) VALUES('%s','%s','%s','%d','%d','%d')"%(s_name,air_quality['longitude'][0:6]+"_0"+air_quality['latitude'][0:5],now.strftime('%Y%m%d%H%M%S'),int(air_quality['aqi']),int(air_quality['pm2_5']),int(air_quality['pm10'])))
-            conn.commit()
-            path="D:\\air_quality\\%s.txt"%s_name
-            with open(path,'a') as f:
-                f.write(air_quality['aqi']+"   "+air_quality['pm2_5']+"   "+air_quality['pm10']+"   "+now.strftime('%Y%m%d%H%M%S')+'\n')
-        cur.close()
-        conn.close()
+    while(True):
+        crawl_air_quality_beijing()
+        crawl_air_quality_bupt()
         time.sleep(inc)
+
 
 crawl_air_quality(1800)
